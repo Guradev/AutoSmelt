608911274967c2c9d35a5fcf9fdfeb11c091a191
Gura <contactogura1@gmail.com>
2025-07-04 21:16:17 -0500

    Added changes

--- src/main/java/net/gura/autoSmelt/AutoSmelt.java
+++ src/main/java/net/gura/autoSmelt/AutoSmelt.java
@@ -1,5 +1,6 @@
 package net.gura.autoSmelt;
 
+import net.gura.autoSmelt.listener.AutoSmeltListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AutoSmelt extends JavaPlugin {
@@ -7,11 +8,13 @@ public final class AutoSmelt extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new AutoSmeltListener(), this);
+        getServer().getConsoleSender().sendMessage("[AutoSmelt] Plugin enabled");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage("[AutoSmelt] Plugin disabled");
     }
 }
--- src/main/java/net/gura/autoSmelt/listener/AutoSmeltListener.java
+++ src/main/java/net/gura/autoSmelt/listener/AutoSmeltListener.java
@@ -1,4 +1,87 @@
 package net.gura.autoSmelt.listener;
 
-public class AutoSmeltListener {
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.*;
+
+public class AutoSmeltListener implements Listener {
+
+    private final static Map<Material, Material> SMELT_MAP = new HashMap<>();
+
+    static {
+        SMELT_MAP.put(Material.IRON_ORE, Material.IRON_INGOT);
+        SMELT_MAP.put(Material.GOLD_ORE, Material.GOLD_INGOT);
+        SMELT_MAP.put(Material.DIAMOND_ORE, Material.DIAMOND);
+        SMELT_MAP.put(Material.EMERALD_ORE, Material.EMERALD);
+        SMELT_MAP.put(Material.REDSTONE_ORE, Material.REDSTONE);
+    }
+
+    private static final Set<Material> FORTUNE_COMPATIBLE = EnumSet.of(
+            Material.COAL_ORE,
+            Material.DIAMOND_ORE,
+            Material.EMERALD_ORE,
+            Material.REDSTONE_ORE,
+            Material.GLOWING_REDSTONE_ORE,
+            Material.QUARTZ_ORE
+    );
+
+    private int random(int min, int max) {
+        return new Random().nextInt(max - min + 1) + min;
+    }
+
+    private int getXpBlock(Material type) {
+        switch (type) {
+            case COAL_ORE: return random(0, 2);
+            case DIAMOND_ORE:
+            case EMERALD_ORE: return random(3, 7);
+            case REDSTONE_ORE:
+            case GLOWING_REDSTONE_ORE: return random(1, 5);
+            case QUARTZ_ORE: return random(2, 5);
+            case IRON_ORE:
+            case GOLD_ORE: return 1;
+            default: return 0;
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+        Material type = block.getType();
+        ItemStack item = player.getItemInHand();
+
+        if (item != null && item.containsEnchantment(Enchantment.SILK_TOUCH)) return;
+
+        if (!SMELT_MAP.containsKey(type)) return;
+
+        event.setCancelled(true);
+        block.setType(Material.AIR);
+
+        Material dropType = SMELT_MAP.get(type);
+        int dropAmount = 1;
+
+        if (FORTUNE_COMPATIBLE.contains(type)) {
+            int fortuneLevel = item.getEnchantmentLevel(Enchantment.LOOT_BONUS_BLOCKS);
+            if (fortuneLevel > 0) {
+                int bonus = new Random().nextInt(fortuneLevel + 2) - 1;
+                dropAmount += Math.max(bonus, 0);
+            }
+        }
+
+        ItemStack drop = new ItemStack(dropType, dropAmount);
+        block.getWorld().dropItemNaturally(block.getLocation(), drop);
+
+        int xp = getXpBlock(type);
+        block.getWorld().spawn(block.getLocation(), ExperienceOrb.class).setExperience(xp);
+
+    }
+
 }
